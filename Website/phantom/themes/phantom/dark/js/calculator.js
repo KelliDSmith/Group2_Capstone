8f7db5e575b8663e2aea9f22bcf7c304d7213524
Kelli Smith <84884700+KelliDSmith@users.noreply.github.com>
2021-11-22 00:07:32 -0600

    website update

---
+++ Website/phantom/themes/phantom/dark/js/calculator.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("#submit").on("click",function() {
+        inputdata();
+    });
+});
+
+function inputdata () {
+    let job_title = $("#job_title");
+    let gender = $("#gender");
+    let department = $("#department");
+    let seniority = $("#seniority");
+
+    
+
+}
